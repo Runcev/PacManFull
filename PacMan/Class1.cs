f28b8ba17506e8e6b0b3abaf8965258f8bed1fd0
Zhenia Borodaikevych <borodaykevich@gmail.com>
2020-10-01 13:27:09 +0300

    added simple game bootstrapping

--- PacMan/Class1.cs
+++ PacMan/Class1.cs
@@ -1,8 +1,22 @@
-﻿using System;
+﻿using Otter.Core;
+using Otter.Graphics;
+using Otter.Graphics.Drawables;
 
 namespace PacMan
 {
     public class Class1
     {
+        public static void Main(string[] args)
+        {
+            var game = new Game("Scenes and Entities");
+            
+            var testEntity = new Entity(350, 240);
+            testEntity.AddGraphic(Image.CreateCircle(100, Color.Red));
+            
+            var scene = new Scene();
+            scene.Add(testEntity);
+            
+            game.Start(scene);
+        }
     }
 }
